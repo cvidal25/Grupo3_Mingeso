7d03cbf2fc696b0a647e1f7d9f5834d2631be792
cvidal <christian.vidal@usach.cl>
2018-05-06 22:14:25 -0300

    add backendProject

---
+++ backend/src/main/java/grupo3/mingeso/MingesoApplication.java
@@ -0,0 +1,12 @@
+package grupo3.mingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingesoApplication.class, args);
+	}
+}
