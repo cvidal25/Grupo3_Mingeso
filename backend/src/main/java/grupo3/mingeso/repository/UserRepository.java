dca5322301d2341e4538bec483a68998017b4188
Unknown <christian.vidal@usach.cl>
2018-05-08 16:22:00 -0300

    add id autoincrement

--- backend/src/main/java/grupo3/mingeso/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package grupo3.mingeso.repository;
-
-import grupo3.mingeso.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<User,Integer> {
-    User findByEmail(String email);
-}
