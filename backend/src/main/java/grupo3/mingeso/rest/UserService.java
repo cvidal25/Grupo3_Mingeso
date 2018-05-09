4e971fed6f6c00e99054588abf92b660e70065da
Shalini Ramchandani <shalini.ramchandani@usach.cl>
2018-05-09 14:56:48 -0300

    excercise repository working, still working on user

--- backend/src/main/java/grupo3/mingeso/rest/UserService.java
+++ backend/src/main/java/grupo3/mingeso/rest/UserService.java
@@ -1,56 +1,55 @@
 package grupo3.mingeso.rest;
-import grupo3.mingeso.repository.ExerciseRepository;
-import grupo3.mingeso.entities.Exercise;
 
+import grupo3.mingeso.repository.UserRepository;
+import grupo3.mingeso.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
+
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
-@RequestMapping("/exercise")
-public class ExerciseService {
+@RequestMapping("/user")
+public class UserService {
 
     @Autowired
-    ExerciseRepository exerciseRepository;
-    //GET ALL
+    UserRepository userRepository;
+
+    //Get all
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public Iterable<Exercise> getAllExercise() {
-        return exerciseRepository.findAll();
-    }
+    public Iterable<User> getAllUsers(){ return userRepository.findAll(); }
 
-    //GET ONE
+    //Get one
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public Optional<Exercise> findOne(@PathVariable("id") Integer id) {
-        return exerciseRepository.findById(id);
+    public Optional<User> findOne(@PathVariable("id") Integer id) {
+        return userRepository.findById(id);
     }
 
     //CREATE ONE
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public Exercise create(@RequestBody Exercise resource) {
+    public User create(@RequestBody User resource) {
 
-        return exerciseRepository.save(resource);
+        return userRepository.save(resource);
     }
 
     //DELETE ONE
-    @DeleteMapping(value = "delete/{id}")
+    @DeleteMapping(value = "user/{id}")
     @ResponseBody
     public void productDelete(@PathVariable Integer id){
-        exerciseRepository.deleteById(id);
+        userRepository.deleteById(id);
 
     }
 
     //UPDATE
-    @PutMapping(value = "/update", params = {"id","body"})
+    /*@PutMapping(value = "/user", params = {"id","body"})
     public Exercise update(@RequestParam("id") Integer id, @RequestParam("body") String body,
-                          HttpServletResponse httpResponse) {
+                           HttpServletResponse httpResponse) {
 
         if(!exerciseRepository.existsById(id)) {
             httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
@@ -60,7 +59,5 @@ public class ExerciseService {
         Exercise exercise = exerciseRepository.findById(id).get();
         exercise.setExerciseBody(body);
         return exerciseRepository.save(exercise);
-    }
-
-
+    }*/
 }
