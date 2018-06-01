2ebb8b308e0ffc01355de76e72dd3ff1d717e8d1
JORGE <jorge.paredes@usach.cl>
2018-06-01 16:46:46 -0400

    modificado el formulario

--- front-end/src/views/Componentes/Enunciado/NewEnunciado.js
+++ front-end/src/views/Componentes/Enunciado/NewEnunciado.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import { Card, CardBody, CardHeader, Col, Row, Input } from 'reactstrap';
+import { Card, CardBody, CardHeader, Col, Row, Input,DropdownItem,DropdownMenu,
+    DropdownToggle,Form,FormGroup,FormText,FormFeedback,InputGroup,InputGroupAddon,
+    InputGroupText,Label,Button,ButtonDropdown,} from 'reactstrap';
+import { AppSwitch } from '@coreui/react';
 import Axios from 'axios';
 
 class NewEnunciado extends Component{
@@ -13,11 +16,16 @@ class NewEnunciado extends Component{
             fechaInicial:"",
             fechaFinal:"",
             PuntosTotales:1,
-            entradas:"",
-            salidas:"",
+            dificultad:1,
+            entradas:[],
+            salidas:[],
+            tempEntrada:"",
+            tempSalida:"",
+            publicar:false,
             aceptar:false,
 
         };
+        
     }
     componentDidMount(){
         
@@ -43,21 +51,90 @@ class NewEnunciado extends Component{
             })
         }*/
     }
-    stringToFormatInOut(texto){
-        texto=texto.split("\n");
+    handleClickPublicar=event=>{
+        console.log(event.target);
+        var name=event.target.name;
+        var value=event.target.value;
+       this.setState({
+           [name]:!this.state.publicar
+       });
+    }
+    
+    handleInsertArray=nombreInOut=>{
+            console.log(nombreInOut);
+        var InOut;
+        if(nombreInOut==="entrada"){
+            InOut=this.state.entradas;
+            //Validador
+            if(this.state.tempEntrada===""){
+                //decir que esta vacio
+                return;
+            }
+
+            InOut.push(this.state.tempEntrada);
+            this.setState({
+                entradas:InOut,
+                tempEntrada:""
+            });
+        }
+        else{
+            InOut=this.state.salidas;
+            //validador
+            if(this.state.tempSalida===""){
+                //decir que esta vacio
+                return;
+            }
+            InOut.push(this.state.tempSalida);
+            this.setState({
+                salidas:InOut,
+                tempSalida:""
+            });
+        }
+        
+    }
+
+    handleDeleteArray=(which,posicion)=>{
+        console.log(posicion);
+        var array=[];
+        switch (which){
+            case 1: //entrada
+            
+                array=this.state.entradas;
+                console.log(array);
+                array.splice(posicion,1);
+                console.log(array);
+                this.setState({
+                    entradas:array
+                });
+                break;
+            
+            case 2: //salida
+                array=this.state.salidas;
+                array.splice(posicion,1);
+                this.setState({
+                    salidas:array
+                });
+                break;
+            default:
+                return;
+        }
+
+        
+    }
+
+    arrayToFormatInOut(array){
         var aux='';
         var complete='';
-        for(aux in texto){
+        for(aux in array){
             if(complete===''){
-                complete=texto[aux];
+                complete=array[aux];
             }
             else{
-                complete=complete+'/@'+texto[aux]
+                complete=complete+'/@'+array[aux]
             }
         }
         console.log(complete);
         return complete;
-
     }
 
     CrearEnunciadoAPI=event=>{
@@ -85,6 +162,24 @@ class NewEnunciado extends Component{
         console.log(exercise);*/
     }
 
+    //Entrada: Arreglo
+    ListarInOut(InOut,which){
+        return (
+            InOut && InOut.map((dato,key)=>
+            <Row key={key}>
+                <Col md={10} style={{width:"80%"}}>
+                    <Input  disabled value={dato}/>
+                    </Col>
+                <Col md={1} style={{width:"10%",textAlign:"left"}}>
+                    <Label>
+                        <i id={key.toString()} className="btn-pill fa fa-close fa-lg btn-outline-danger font-3xl"
+                         onClick={()=>{this.handleDeleteArray(which,key);}} ></i>
+                    </Label>
+                </Col>
+            </Row>
+            ));
+    }
+
     render(){
         var ej1="ejemplo:\n2\n4\n8";
         var ej2="ejemplo:\n4\n16\n64";
@@ -99,78 +194,132 @@ class NewEnunciado extends Component{
                     </CardHeader>
                     <CardBody>
                     <form>
-                        <div className="form-group">
-                            <label >Titulo</label>
-                            <Input required type="text" className="form-control" name="titulo" id="tituloEnun" aria-describedby="" placeholder="Introduce el titulo del Enunciado" onChange={this.handleChange}/>
-                        </div>
-                        <div className="form-group">
-                            <label >Enunciado</label>
-                            <textarea required className='form-control form-control' name="enunciado" id="bodyEnunciado" rows="10" style={{resize:'none'}} placeholder="Introduce el Cuerpo del Enunciado" onChange={this.handleChange}></textarea>
-                        </div>
-
-                        <div className="form-group"  >
-                            <label >Lenguaje</label>
-                            <div></div>
-                            <div className="form-check form-check-inline">
-                                <Input required className="form-check-input" type="radio" name="lenguaje" id="inlineRadio1" value="1" defaultChecked onClick={this.handleChange}/>
-                                <label className="form-check-label">Python</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <Input required className="form-check-input" type="radio" name="lenguaje" id="inlineRadio2" value="2" disabled onClick={this.handleChange}/>
-                                <label className="form-check-label" >Java</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <Input required className="form-check-input" type="radio" name="lenguaje" id="inlineRadio3" value="3" disabled onClick={this.handleChange}/>
-                                <label className="form-check-label" >C </label>
-                            </div>
-                        </div>
-                        
-                        <div className="row">
-                            <div className="form-group col">
-                                <label >Entradas</label>
-                                <textarea required className="form-control" name="entradas" id="entradas" aria-describedby="emailHelp" placeholder={ej1} rows="5" onChange={this.handleChange} style={{resize:'none'}}/>
-                                <small id="emailHelp" className="form-text text-muted">Las entradas deben estar separadas el salto de linea, y debe corresponder a la misma en la salidas.</small>
-                            </div>
-
-                            <div className="form-group col">
-                                <label >Salidas</label>
-                                <textarea required className="form-control" id="salidas" name="salidas" aria-describedby="emailHelp" placeholder={ej2} rows="5" onChange={this.handleChange} style={{resize:'none'}}/>
-                                <small id="emailHelp" className="form-text text-muted">Las salidas deben estar separadas el salto de linea, y deben corresponder a la misma con las entradas.</small>
-                            </div>
-                        </div>
-        
-
-
-                        <div className="row">
-                            <div className="col form-group">
-                                <label >Fecha inicial</label>
-                                <div></div>
-                                <Input required type="date" id="fechaIni" name='fechaInicial'  onChange={this.handleChange}/>
-                            </div>
-                            <div className="col form-group">
-                                <label>Fecha Final</label>
-                                <div></div>
-                                <Input required type="date" id="fechaFinal" name='fechaFinal' style={{width:'100%'}} onChange={this.handleChange}/>
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="form-group col">
-                            <label>Puntaje Total del Enunciado</label>
-                                <Input required type="number" name="PuntosTotales" className="form-control" min="1" pattern="[0-9]" id="puntosTotales" aria-describedby="" defaultValue={1} onChange={this.handleChange}/>
-                            </div>
-                            <div className="col"></div>
-                        </div>
-                        <div>
+                        <FormGroup>
+                            <Label  htmlFor="tituloEnun">Titulo</Label>
+                            <Input required type="text" name="titulo" id="tituloEnun" aria-describedby="info" 
+                            placeholder="Introduce el titulo del Enunciado" onChange={this.handleChange}/>
                             
-                            </div>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor="bodyEnunciado">Enunciado</Label>
+                            <Input type="textarea" required name="enunciado" id="bodyEnunciado" rows="10" style={{resize:'none'}} placeholder="Introduce el Cuerpo del Enunciado" onChange={this.handleChange}/>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Label>Lenguaje</Label>
+                            <div></div>
+                            <FormGroup check inline>
+                                <Input required className="form-check-input" type="radio" name="lenguaje" id="inlineRadio1" value="1" defaultChecked onClick={this.handleChange}/>
+                                <Label check htmlFor="inlineRadio1">Python</Label>
+                            </FormGroup>
+                            <FormGroup check inline>
+                                <Input required className="form-check-input" type="radio" name="lenguaje" id="inlineRadio2" value="2" onClick={this.handleChange}/>
+                                <Label check htmlFor="inlineRadio2">Java</Label>
+                            </FormGroup>
+                            <FormGroup check inline>
+                                <Input required className="form-check-input" type="radio" name="lenguaje" id="inlineRadio3" value="3" onClick={this.handleChange}/>
+                                <Label check htmlFor="inlineRadio3">C </Label>
+                            </FormGroup>
+                        </FormGroup>
+
+                        
+                        <Row >
+                            <Col md={6}>
+                                <Label  htmlFor="entrada">Entradas</Label>
+                                {this.ListarInOut(this.state.entradas,1)}
+                                <Row>
+                                    <Col md={10} style={{width:"80%"}}>
+                                        <Input value={this.state.tempEntrada} name="tempEntrada" id="entrada" onChange={this.handleChange}/>
+                                    </Col>
+                                    <Col md={1} style={{width:"10%",textAlign:"left"}}>
+                                        <Label onClick={()=>{this.handleInsertArray("entrada");}} >
+                                            <i className="btn-pill fa fa-plus-circle fa-lg btn-outline-info font-5xl"
+                                            style={{ color: '#89e4ff'}} ></i>
+                                        </Label>
+                                    </Col>
+                                </Row>
+                                <small id="emailHelp" className="form-text text-muted">Las entradas deben estar separadas el salto de linea, y debe corresponder a la misma en la salidas.</small>
+                                 
+                            </Col>
+                            
+                            <Col md={6} >
+                                <Label htmlFor="salida" >Salidas</Label>
+                                {this.ListarInOut(this.state.salidas,2)}
+                                <Row>
+                                    <Col md={10} style={{width:"80%"}}>
+                                        <Input value={this.state.tempSalida} name="tempSalida" id="salida" onChange={this.handleChange}/>
+                                        </Col>
+                                    <Col md={1} style={{width:"10%",textAlign:"left"}}>
+                                        <Label onClick={()=>{this.handleInsertArray("salida");}} >
+                                            <i className="btn-pill fa fa-plus-circle fa-lg btn-outline-info font-5xl"
+                                            style={{ color: '#89e4ff'}} ></i>
+                                        </Label>
+                                    </Col>
+                                </Row>
+                                <small id="emailHelp" className="form-text text-muted">Las salidas deben estar separadas el salto de linea, y deben corresponder a la misma con las entradas.</small>
+                            </Col>
+                        </Row>
+                        
                         <br/>
-                        <br/>
+                        <FormGroup>
+                            <Label>Dificultad</Label>
+                            <div></div>
+                            <FormGroup check inline>
+                                <Input required className="form-check-input" type="radio" name="dificultad" id="inlineRadio1" value="1" defaultChecked onClick={this.handleChange}/>
+                                <Label check htmlFor="inlineRadio1">Fácil</Label>
+                            </FormGroup>
+                            <FormGroup check inline>
+                                <Input required className="form-check-input" type="radio" name="dificultad" id="inlineRadio2" value="2" onClick={this.handleChange}/>
+                                <Label check htmlFor="inlineRadio2">Medio</Label>
+                            </FormGroup>
+                            <FormGroup check inline>
+                                <Input required className="form-check-input" type="radio" name="dificultad" id="inlineRadio3" value="3" onClick={this.handleChange}/>
+                                <Label check htmlFor="inlineRadio3">Difícil </Label>
+                            </FormGroup>
+                        </FormGroup>
+
+                        <Row>
+                            <Col>
+                            <Row>
+                                <Col md={10} style={{width:"80%"}}>
+                                <FormGroup >
+                                    <Label  htmlFor="dias">Días para Realizar</Label>
+                                    <Input required type="number" id="dias" name="PuntosTotales" className="form-control" min="0" pattern="[0-9]" defaultValue={0} onChange={this.handleChange}/>
+                                </FormGroup>
+                                </Col>
+                                </Row>
+                            </Col>
+                            <Col>
+                            <Row>
+                                <Col md={10} style={{width:"80%"}}>
+                                <FormGroup >
+                                    <Label htmlFor="puntosTotales">Puntaje Total del Enunciado</Label>
+                                    <Input required type="number" name="PuntosTotales" className="form-control" min="1" pattern="[0-9]" id="puntosTotales"  defaultValue={1} onChange={this.handleChange}/>
+                                </FormGroup>
+                                </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+
+                        <FormGroup row>
+                            <Col md="2"><Label htmlFor="publicar">Publicar Ahora</Label></Col>
+                            <Col style={{float:"right"}}>
+                                <FormGroup check className="checkbox">
+                                    <Input className="form-check-input" type="checkbox" id="publicar" name="publicar" 
+                                    value={this.state.publicar} checked={this.state.publicar} 
+                                    onClick={this.handleClickPublicar}/>
+                                </FormGroup>
+                            </Col>
+                        </FormGroup>
+                        
+                    
+
                         
                     </form>
                     <button  onClick={this.CrearEnunciadoAPI} disabled={this.state.aceptar}>
-                        <strong onClick={this.CrearEnunciadoAPI}> Crear</strong>
-                            </button>
+                            <strong> Crear</strong>
+                        </button>
+                    
                     </CardBody>
                 </Card>
                   </Col>  
