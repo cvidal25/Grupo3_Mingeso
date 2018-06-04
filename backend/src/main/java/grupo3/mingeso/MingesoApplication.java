55d1c9ff43c2929b72e87959cfef6d4f4501a32a
Unknown <christian.vidal@usach.cl>
2018-06-04 12:48:18 -0400

    Fix Deploy in Tomcat

--- backend/src/main/java/grupo3/mingeso/MingesoApplication.java
+++ backend/src/main/java/grupo3/mingeso/MingesoApplication.java
@@ -2,11 +2,20 @@ package grupo3.mingeso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.util.StringUtils;
+import grupo3.mingeso.others.CodeAnalysis;
 
 @SpringBootApplication
-public class MingesoApplication {
+public class MingesoApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MingesoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MingesoApplication.class);
 	}
 }
