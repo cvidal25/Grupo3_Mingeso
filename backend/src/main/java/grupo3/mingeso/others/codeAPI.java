a88a3a00ac166c9ba46bbdfcc66738c8c6ca125f
Shalini Ramchandani <shalini.ramchandani@usach.cl>
2018-06-03 20:29:00 -0400

    Deleting code duplications

---
+++ backend/src/main/java/grupo3/mingeso/others/codeAPI.java
@@ -0,0 +1,80 @@
+package grupo3.mingeso.others;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class codeAPI {
+    public codeAPI(){
+    }
+
+    public String runCode(String code, String language){
+        try {
+            URL url;
+            if(language.equals("python")){
+                url = new URL("https://run.glot.io/languages/"+language+"/2");
+            }else{
+                url = new URL("https://run.glot.io/languages/"+language+"/latest");
+            }
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("authorization", "Token b5638b86-3d1c-44e4-b152-29a69cf1cfde");
+
+            String realCode;
+            String programName = "";
+            if(language.equals("python")){
+                programName = "main.py";
+            }else if(language.equals("java")){
+                programName = "Main.java";
+            }else if(language.equals("c")){
+                programName = "main.c";
+            }
+
+            realCode = "{\"files\":\n" +
+                    "\t[\n" +
+                    "\t\t{\"name\": \""+programName+"\", \n" +
+                    "\t\"content\": \""+code+"\"}\n" +
+                    "\t]\n" +
+                    "\t\n" +
+                    "}";
+            // String code = "{\"files\": [{\"name\" : \"main.py\", \"content\": \"" + input +"\"}]}";
+
+            System.out.println(realCode);
+
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(realCode.getBytes());
+            outputStream.flush();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new RuntimeException("Please check your inputs : HTTP error code : "+ connection.getResponseCode());
+            }
+
+            BufferedReader bufferedReader;
+            bufferedReader = new BufferedReader(new InputStreamReader(
+                    (connection.getInputStream())));
+
+            String output;
+            String realOutput = "";
+            System.out.println("Output from Glot .... \n");
+            while ((output = bufferedReader.readLine()) != null) {
+                realOutput += output;
+                System.out.println(output);
+            }
+            connection.disconnect();
+            return realOutput;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
