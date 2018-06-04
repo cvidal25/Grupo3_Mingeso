830f3ce98ff64a6c97b57faedd70deecab82bbfb
Christian Vidal <christian.vidal@usach.cl>
2018-06-04 12:54:10 -0400

    Update MingesoApplication.java

--- backend/src/main/java/grupo3/mingeso/MingesoApplication.java
+++ backend/src/main/java/grupo3/mingeso/MingesoApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.util.StringUtils;
-import grupo3.mingeso.others.CodeAnalysis;
 
 @SpringBootApplication
 public class MingesoApplication  extends SpringBootServletInitializer {
