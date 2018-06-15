33900ce73b0196fde83d82a5d89cd4adf13895be
Unknown <christian.vidal@usach.cl>
2018-06-15 00:14:08 -0400

    Modify Code Analysis

--- backend/src/main/java/grupo3/mingeso/others/CodeAnalysis.java
+++ backend/src/main/java/grupo3/mingeso/others/CodeAnalysis.java
@@ -5,187 +5,209 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.*;
+import static org.apache.commons.lang3.StringUtils.endsWithIgnoreCase;
 
 
 public class CodeAnalysis {
 
-    public void searchCommentsCode(String code, Integer lenguage) {
+    //Busca Comentarios de la Funcion.
+    //Detecta la Funcion.
+    //Detecta Comentario Input
+    //Detecta Comentario Output
+    //Detecta Comentario Descripcion.
+    // PYTHON C JAVA
+    public boolean searchCommentsCode(String code, Integer lenguage) {
         if (lenguage == 1) {
-            int occurance = StringUtils.countMatches(code.toLowerCase(), "def");
-            int input = StringUtils.countMatches(code.toLowerCase(), "#input");
-            int out = StringUtils.countMatches(code.toLowerCase(), "#output");
-            int description = StringUtils.countMatches(code.toLowerCase(), "#descripcion");
+            int occurance = countMatches(code.toLowerCase(), "def");
+            int input = countMatches(code.toLowerCase(), "#input");
+            int out = countMatches(code.toLowerCase(), "#output");
+            int description = countMatches(code.toLowerCase(), "#descripcion");
             if (occurance == input && occurance == out && occurance == description) {
-                System.out.println("Paso el test de Comentario");
+                return true;
             } else {
-                System.out.println("el codigo no pasa el test de comentario");
+                return false;
             }
         }
         if (lenguage == 2) {
             int function = 0;
-            int input = StringUtils.countMatches(code.toLowerCase(), "//input");
-            int out = StringUtils.countMatches(code.toLowerCase(), "//output");
-            int description = StringUtils.countMatches(code.toLowerCase(), "//descripcion");
+            int input = countMatches(code.toLowerCase(), "//input");
+            int out = countMatches(code.toLowerCase(), "//output");
+            int description = countMatches(code.toLowerCase(), "//descripcion");
             List<String> myList = new ArrayList<String>(Arrays.asList(code.split("\n")));
             for (String elemento : myList) {
                 if (elemento.contains("public") || elemento.contains("private") || elemento.contains("protected")) {
-                    System.out.println(elemento);
                     function++;
                 }
 
             }
             if (function == input && function == out && function == description) {
-                System.out.println("Cumple con el punto (C)");
+               return true;
             } else {
-                System.out.println("No cumple el punto(C)");
+               return false;
             }
         }
 
         if (lenguage == 3) {
-            int input = StringUtils.countMatches(code.toLowerCase(), "//input");
-            int out = StringUtils.countMatches(code.toLowerCase(), "//output");
-            int description = StringUtils.countMatches(code.toLowerCase(), "//descripcion");
+            int input = countMatches(code.toLowerCase(), "//input");
+            int out = countMatches(code.toLowerCase(), "//output");
+            int description = countMatches(code.toLowerCase(), "//descripcion");
             int function = 0;
             List<String> myList = new ArrayList<String>(Arrays.asList(code.split("\n")));
             for (String elemento : myList) {
                 if (elemento.contains("int ") || elemento.contains("float ") || elemento.contains("double ") || elemento.contains("char ") || elemento.contains("void ")) {
-                    if (StringUtils.endsWithIgnoreCase(elemento, ")") || StringUtils.endsWithIgnoreCase(elemento, "{")) {
+                    if (endsWithIgnoreCase(elemento, ")") || endsWithIgnoreCase(elemento, "{")) {
                         function++;
-                        System.out.println(elemento);
                     }
 
                 }
 
             }
             if (function == input && function == out && function == description) {
-                System.out.println("Cumple con el punto (C)");
+               return true;
             } else {
-                System.out.println("No cumple el punto(C)");
+               return false;
             }
         }
 
+        return false;
 
     }
 
-    public void detectVariable(String code) {
+    //Detect Variables para C y JAVA
+    //IDENTIFIED LARGO
+    public List<String> detectVariable(String code) {
 
         List<String> myList = new ArrayList<String>(Arrays.asList(code.split("\n")));
-        System.out.println(myList);
+        List<String> smallVariables = new ArrayList<>();
 
         for (String linea : myList) {
-
             if (linea.contains("int ")) {
-                //hacer para cada tipo de variable solo se cambia el primer replace
-                if (StringUtils.endsWithIgnoreCase(linea, ";")) {
-
-                    String aux = StringUtils.substringBefore(StringUtils.deleteWhitespace(linea.replace("int ", "").replace(";", "")), "=");
-                    System.out.println(aux);
-                    System.out.println(aux.length());
+                if (endsWithIgnoreCase(linea, ";")) {
+                    String aux = substringBefore(deleteWhitespace(linea.replace("int ", "").replace(";", "")), "=");
+                    lenVariableAnalyzer(smallVariables,aux);
                 }
             }
-
             if (linea.contains("float ")) {
-                //hacer para cada tipo de variable solo se cambia el primer replace
-                if (StringUtils.endsWithIgnoreCase(linea, ";")) {
-
-                    String aux = StringUtils.substringBefore(StringUtils.deleteWhitespace(linea.replace("float ", "").replace(";", "")), "=");
-                    System.out.println(aux);
-
-                    System.out.println(aux.length());
+                if (endsWithIgnoreCase(linea, ";")) {
+                    String aux = substringBefore(deleteWhitespace(linea.replace("float ", "").replace(";", "")), "=");
+                    lenVariableAnalyzer(smallVariables,aux);
                 }
             }
             if (linea.contains("double ")) {
-                //hacer para cada tipo de variable solo se cambia el primer replace
-                if (StringUtils.endsWithIgnoreCase(linea, ";")) {
-
-                    String aux = StringUtils.substringBefore(StringUtils.deleteWhitespace(linea.replace("double ", "").replace(";", "")), "=");
-                    System.out.println(aux);
-
-                    System.out.println(aux.length());
+                if (endsWithIgnoreCase(linea, ";")) {
+                    String aux = substringBefore(deleteWhitespace(linea.replace("double ", "").replace(";", "")), "=");
+                    lenVariableAnalyzer(smallVariables,aux);
                 }
             }
             if (linea.contains("char ")) {
-                //hacer para cada tipo de variable solo se cambia el primer replace
-                if (StringUtils.endsWithIgnoreCase(linea, ";")) {
-
-                    String aux = StringUtils.substringBefore(StringUtils.deleteWhitespace(linea.replace("char ", "").replace(";", "").replace("*", "")), "=");
-                    System.out.println(aux);
-
-                    System.out.println(aux.length());
+                if (endsWithIgnoreCase(linea, ";")) {
+                    String aux = substringBefore(deleteWhitespace(linea.replace("char ", "").replace(";", "").replace("*", "")), "=");
+                    lenVariableAnalyzer(smallVariables,aux);
                 }
             }
 
-
         }
+        return  smallVariables;
     }
 
-    public void dectectVariablePython(String code) {
-        List<String> myList = new ArrayList<String>(Arrays.asList(code.split("\n")));
-        for (String elemento : myList) {
+    //Detecta Variables para Python
+    // IDentifica largo.
+    public List<String> dectectVariablePython(String code) {
+        List<String> myList = new ArrayList<>(Arrays.asList(code.split("\n")));
+        List<String> smallVariables = new ArrayList<>();
+        for (String elemento : myList)
             if (elemento.contains("=")) {
-                String aux = StringUtils.substringBefore(StringUtils.deleteWhitespace(elemento), "=");
+                String aux = substringBefore(deleteWhitespace(elemento), "=");
                 if (aux.length() < 3) {
-                    System.out.println(aux);
-
-                } else {
-                    System.out.println("cumple");
+                    lenVariableAnalyzer(smallVariables,aux);
                 }
-
             }
-        }
+        return smallVariables;
+    }
 
+    public void  lenVariableAnalyzer(List<String> smallVariables,String variable){
+        if (variable.length()<3){
+            smallVariables.add(variable);
+        }
 
     }
 
-    public void findPrincipalBody(String code, Integer lenguage) {
+    //Detecta Cuerpo principal
+    public boolean findPrincipalBody(String code, Integer lenguage) {
         if (lenguage == 1) {
-            int occuranceInput = StringUtils.countMatches(code.toLowerCase(), "#entrada");
-            int occuranceProcess = StringUtils.countMatches(code.toLowerCase(), "#procesamiento");
-            int occuranceOutput = StringUtils.countMatches(code.toLowerCase(), "#salida");
+            int occuranceInput = countMatches(code.toLowerCase(), "#entrada");
+            int occuranceProcess = countMatches(code.toLowerCase(), "#procesamiento");
+            int occuranceOutput = countMatches(code.toLowerCase(), "#salida");
             if (occuranceInput == 1 && occuranceProcess == 1 && occuranceOutput == 1) {
-                System.out.println("El codigo cumple con el cuerpo principal");
+               return true;
 
             } else {
-                System.out.println("El codigo No cumple con el cuerpo principal");
+               return false;
 
             }
         }
         if (lenguage == 2 || lenguage == 3) {
-            int occuranceInput = StringUtils.countMatches(code.toLowerCase(), "//entrada");
-            int occuranceProcess = StringUtils.countMatches(code.toLowerCase(), "//procesamiento");
-            int occuranceOutput = StringUtils.countMatches(code.toLowerCase(), "//salida");
+            int occuranceInput = countMatches(code.toLowerCase(), "//entrada");
+            int occuranceProcess = countMatches(code.toLowerCase(), "//procesamiento");
+            int occuranceOutput = countMatches(code.toLowerCase(), "//salida");
             if (occuranceInput == 1 && occuranceProcess == 1 && occuranceOutput == 1) {
-                System.out.println("El codigo cumple con el cuerpo principal");
+             return true;
 
             } else {
-                System.out.println("El codigo No cumple con el cuerpo principal");
+               return false;
 
             }
         }
-
-
+        return false;
     }
 
 
+    public boolean identationAnalyzer(String code) {
 
-    public void IdentationAnalyzer(String code) {
-
-
-        int identation = StringUtils.countMatches(code.toLowerCase(), "  ");
-        int tabulacion = StringUtils.countMatches(code.toLowerCase(), "\t");
-        if (identation > 1 || tabulacion > 1) {
-            System.out.println("Cumnple identacion");
+        int identation = countMatches(code.toLowerCase(), "  ");
+        int tabulation = countMatches(code.toLowerCase(), "\t");
+        if (identation >= 1 || tabulation >= 1) {
+            return true;
         }
         else {
-            System.out.println("Terrible pollo ");
+            return false;
+        } }
+
+    public HashMap<String,String> Analysis(String code, Integer lenguage){
+        HashMap<String,String> codeAnswer = new HashMap<>();
+        if (searchCommentsCode(code,lenguage)){
+            codeAnswer.put("Analisis de Comentario", "Exitoso");
         }
-        System.out.println("Indentation");
-        System.out.println(identation);
+        else{
+            codeAnswer.put("Analisis de Comentario", "Fallido");
+        }
+        if (identationAnalyzer(code)){
+            codeAnswer.put("Analisis de Identación", "Exitoso");
+        }
+        else {
+            codeAnswer.put("Analisis de Identación", "Fallido");
+        }
 
+        if(findPrincipalBody(code,lenguage)){
+            codeAnswer.put("Analisis del Cuerpo Principal", "Exitoso");
+        }
+        else {
+            codeAnswer.put("Analisis del Cuerpo Principal", "Fallido");
+        }
 
+        if (lenguage==1){
+            codeAnswer.put("Variables Invalidas", dectectVariablePython(code).toString());
+        }
+        else if(lenguage==2 || lenguage==3){
+            codeAnswer.put("Variables Invalidas", detectVariable(code).toString());
+        }
+
+        return codeAnswer;
     }
-
 }
 
 
